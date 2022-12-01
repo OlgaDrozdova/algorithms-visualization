6e9a4e331bddcc283e62f556d0abc7882a15ed02
odrozdova <sovunyaolya@gmail.com>
2022-12-01 19:51:12 +0300

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Здесь будут визуализироваться алгоритмы
         </p>
         <a
           className="App-link"
